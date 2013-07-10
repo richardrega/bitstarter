7c986ec3cd2b080d817e7ed7e2327ae1403887a4
Richard Rega <richard.rega@gmail.com>
2013-07-10 01:58:55 +0000

    Trying to Read index.html again

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
+var infile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readFileSync("index.html");
+  var buf = new Buffer(fs.readFileSync(infile);
   response.send(buf.toString("utf-8"));
 });
 
