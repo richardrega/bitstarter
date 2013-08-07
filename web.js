f1e8fdc3a4d0e25b4bea586f8097ec1fcaa2b4c7
Richard <burgundy@burgundy-player.(none)>
2013-08-06 18:33:56 -0700

    Making server adjustments.

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var infile = "index.html";
+var infile = "/home/burgundy/Documents/bitstarter/index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
